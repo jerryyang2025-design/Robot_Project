69868a558c83315d5630db0f5cdd168324af4029
jerryyang2025-design <jerryyang2025@gmail.com>
2026-02-20 17:05:32 -0500

    fixed line following

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ AnalogInputPin right_opto(FEHIO::Pin8);
 AnalogInputPin middle_opto(FEHIO::Pin9);
 AnalogInputPin left_opto(FEHIO::Pin10);
 
-#define LEFT_SPEED 50
-#define RIGHT_SPEED 50
+#define LEFT_SPEED 25
+#define RIGHT_SPEED 25
 
 void ERCMain()
 {
@@ -20,11 +20,11 @@ void ERCMain()
     right.SetPercent(RIGHT_SPEED);
     while (true) {
         if (left_opto.Value() > 3) {
-            left.SetPercent(LEFT_SPEED * 0.6);
-            right.SetPercent(RIGHT_SPEED * 1.3);
+            left.SetPercent(LEFT_SPEED * 1.4);
+            right.SetPercent(RIGHT_SPEED * 0.2);
         } else if (right_opto.Value() > 2.5) {
-            left.SetPercent(LEFT_SPEED * 1.3);
-            right.SetPercent(RIGHT_SPEED * 0.6);
+            left.SetPercent(LEFT_SPEED * 0.2);
+            right.SetPercent(RIGHT_SPEED * 1.4);
         } else {
             left.SetPercent(LEFT_SPEED);
             right.SetPercent(RIGHT_SPEED);
