b8886398f0af82c340bf9380862fc7598268a949
rajeshramshray <rajeshramshray@gmail.com>
2026-02-20 16:48:18 -0500

    Create temp.cpp

---
+++ src/temp.cpp
@@ -0,0 +1,65 @@
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+
+float distance_inches = 14.0; // Distance required for the lab
+float wheel_diameter = 2.5;   // wheel diameter in inches
+float counts_per_rev = 318.0; // encoder counts per revolution
+float pi = 3.14159;
+
+//Declarations for encoders & motors
+DigitalEncoder right_encoder(FEHIO::Pin0);
+DigitalEncoder left_encoder(FEHIO::Pin1);
+FEHMotor right_motor(FEHMotor::Motor0,9.0);
+FEHMotor left_motor(FEHMotor::Motor1,9.0);
+
+void move_forward(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
+void ERCMain()
+{
+    int motor_percent = 25; //Input power level here
+    int expected_counts = 567; //Input theoretical counts here
+
+    float x, y; //for touch screen
+
+    //Initialize the screen
+    LCD.Clear(BLACK);
+    LCD.SetFontColor(WHITE);
+
+    LCD.WriteLine("Shaft Encoder Exploration Test");
+    LCD.WriteLine("Touch the screen");
+    while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+
+    move_forward(motor_percent, expected_counts); //see function
+
+    Sleep(2.0); //Wait for counts to stabilize
+    //Print out data
+    LCD.Write("Theoretical Counts: ");
+    LCD.WriteLine(expected_counts);
+    LCD.Write("Motor Percent: ");
+    LCD.WriteLine(motor_percent);
+    LCD.Write("Actual LE Counts: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("Actual RE Counts: ");
+    LCD.WriteLine(right_encoder.Counts());
+}
+
